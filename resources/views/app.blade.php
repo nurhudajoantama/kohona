0b16a76d89625091143c5bd7dae14ef06afee843
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-08-18 18:53:39 +0700

    feat (icon) : add icon site

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
 
+    <link rel="shortcut icon" href="/assets/icon/favicon.ico" type="image/x-icon">
+
     <!-- Scripts -->
     @routes
     @viteReactRefresh
