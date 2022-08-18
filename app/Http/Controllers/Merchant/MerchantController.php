2d060c0992e20215c1bfc269ef0bb57b21f751c3
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-08-18 22:56:36 +0700

    feat (merchant) : 404 if merchant not active

--- app/Http/Controllers/Merchant/MerchantController.php
+++ app/Http/Controllers/Merchant/MerchantController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Merchant;
 
 use App\Http\Controllers\Controller;
+use App\Models\EnumStatus;
 use App\Models\Merchant;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -11,6 +12,9 @@ class MerchantController extends Controller
 {
     public function show(Merchant $merchant)
     {
+        if ($merchant->status_id !== EnumStatus::activeId) {
+            return abort(404);
+        }
         $merchant->load('products');
         return Inertia::render('Merchant/Show', compact('merchant'));
     }
