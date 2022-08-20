99904057c852e88451b6c70fd77a4357233f3f27
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-08-20 14:38:38 +0700

    fix (modal) : modal modal close

--- resources/js/Components/Modal/Modal.jsx
+++ resources/js/Components/Modal/Modal.jsx
@@ -15,24 +15,34 @@ export default function Modal({ modalOpen, setModalOpen, children }) {
 
     return (
         <main
-            className={`w-full fixed inset-0 bg-slate-600 bg-opacity-30 z-50 transition-opacity cursor-pointer flex justify-center ${
+            className={`fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity ${
                 modalOpen
                     ? "transition ease-out duration-200 opacity-100 block"
                     : "transition ease-out duration-100 opacity-0 hidden"
             }`}
-            onClick={() => {
-                setModalOpen(false);
-            }}
         >
-            <div className="w-full flex justify-center items-center">
+            <div className="w-full flex justify-center items-center mt-44">
                 <div
                     ref={modalContent}
                     className="bg-white overflow-auto max-w-2xl w-full max-h-full rounded shadow-lg px-5 py-2"
                 >
-                    {/* Search form */}
+                    <div className="flex justify-end">
+                        <button
+                            className="border py-0.5 px-2 rounded-md border-gray-300"
+                            onClick={() => setModalOpen(false)}
+                        >
+                            X
+                        </button>
+                    </div>
                     {children}
                 </div>
             </div>
+            <section
+                className="w-screen h-full cursor-pointer fixed"
+                onClick={() => {
+                    setModalOpen(false);
+                }}
+            ></section>
         </main>
     );
 }
