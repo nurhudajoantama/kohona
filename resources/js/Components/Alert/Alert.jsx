1e38e56ea1d012dd2fcd9a82d09bf0d366461abc
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-08-21 17:42:56 +0700

    fix (alert) : fix color of alert

--- resources/js/Components/Alert/Alert.jsx
+++ resources/js/Components/Alert/Alert.jsx
@@ -23,32 +23,45 @@ export default function Alert({ alerts, setAlerts }) {
     };
     return (
         <NoScrollBar className="max-h-32 mb-3 overflow-auto">
-            {alerts.map((alert, i) => (
-                <div
-                    className={`bg-${alert.color}-100 border border-${alert.color}-400 text-${alert.color}-700 px-4 py-3 rounded relative mb-3`}
-                    role="alert"
-                    key={i}
-                >
-                    <strong className="font-bold">{alert.title}</strong>
-                    <span className="block sm:inline ml-2">
-                        {alert.message}
-                    </span>
-                    <button
-                        className="absolute top-0 bottom-0 right-0 px-4 py-3"
-                        onClick={handleRemove(i)}
-                    >
-                        <svg
-                            className={`fill-current h-6 w-6 text-${alert.color}-500`}
-                            role="button"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
+            {alerts.map((alert, i) => {
+                let className = "";
+                switch (alert.color) {
+                    case "red":
+                        className =
+                            "bg-red-100 border border-red-400 text-red-700";
+                        break;
+                    case "yellow":
+                        className =
+                            "bg-yellow-100 border border-yellow-400 text-yellow-700";
+                        break;
+                    default:
+                        className =
+                            "bg-green-100 border border-green-400 text-green-700";
+                }
+                className += " border px-4 py-3 rounded relative mb-3";
+                return (
+                    <div key={i} className={className}>
+                        <strong className="font-bold">{alert.title}</strong>
+                        <span className="block sm:inline ml-2">
+                            {alert.message}
+                        </span>
+                        <button
+                            className="absolute top-0 bottom-0 right-0 px-4 py-3"
+                            onClick={handleRemove(i)}
                         >
-                            <title>Close</title>
-                            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                        </svg>
-                    </button>
-                </div>
-            ))}
+                            <svg
+                                className={`fill-current h-6 w-6 text-${alert.color}-500`}
+                                role="button"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                            >
+                                <title>Close</title>
+                                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                            </svg>
+                        </button>
+                    </div>
+                );
+            })}
         </NoScrollBar>
     );
 }
